25e5343693fd41f76d134f457f88fec12a3d4620
Harpo Barrett <hbar0009@student.monash.edu>
2022-07-16 15:20:40 +1000

    fix: change image downloading to use BookCover30 instead of Book32

--- runme.sh
+++ runme.sh
@@ -6,11 +6,7 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# this snippet here was provided by the book cover dataset
-# available at: https://github.com/uchidalab/book-dataset/blob/master/scripts/download_images.sh
-OUTPUT_DIRPATH="images"
-CSV_FILEPATH="Task2/book32-listing.csv"
-
-python3 download_images.py ${OUTPUT_DIRPATH} ${CSV_FILEPATH}
+python3 download_images.py "data/train" "book30-listing-train.csv"
+python3 download_images.py "data/test" "book30-listing-test.csv"
 
 python3 bookcover_genre_predict.py
