92ad73987a30f3626b94872dc4dd46163473d9e0
Harpo Barrett <hbar0009@student.monash.edu>
2022-07-16 15:35:34 +1000

    feat: add progress messages to runme script

--- runme.sh
+++ runme.sh
@@ -6,7 +6,13 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Finished installing, downloading data"
+
 python3 download_images.py "data/train" "book30-listing-train.csv"
 python3 download_images.py "data/test" "book30-listing-test.csv"
 
+echo "Finished downloading data, now training model"
+
 python3 bookcover_genre_predict.py
+
+echo "Script complete"
